2d55412a341d75753d1a73a32de146fcda7855c9
regencode <thomgozalie@gmail.com>
2026-01-12 13:26:54 +0700

    feat: auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './users/user.module';
+import { AuthModule } from './auth/auth.module';
 import { LlmModule } from './llm/llm.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, LlmModule],
+  imports: [PrismaModule, UserModule, AuthModule, LlmModule],
   controllers: [AppController],
   providers: [AppService],
 })

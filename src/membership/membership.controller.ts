4a93d4d6652efb7cf9b737c8cf646f05b086eba1
regencode <thomgozalie@gmail.com>
2026-01-28 19:35:52 +0700

    feat: admin only router for ~/membership

--- src/membership/membership.controller.ts
+++ src/membership/membership.controller.ts
@@ -5,6 +5,8 @@ import { UpdateMembershipDto } from './dto/update-membership.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import type { ReqUser } from 'src/types/ReqUser';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('me/membership')
 export class MeMembershipController {
@@ -29,21 +31,29 @@ export class MembershipController {
   constructor(private readonly service: MembershipService) {}
 
   @Get(':id')
+  @Roles("ADMIN")
+  @UseGuards(JwtAuthGuard, RolesGuard)
   findOne(@Param('id') id: number) {
     return this.service.findOne(id);
   }
 
   @Post()
+  @Roles("ADMIN")
+  @UseGuards(JwtAuthGuard, RolesGuard)
   create(@Body() dto: CreateMembershipWithUserDto) {
     return this.service.create(dto.userId, dto);
   }
 
   @Patch(':id')
+  @Roles("ADMIN")
+  @UseGuards(JwtAuthGuard, RolesGuard)
   update(@Param('id') id: number, @Body() dto: UpdateMembershipDto) {
     return this.service.update(id, dto);
   }
 
   @Delete(':id')
+  @Roles("ADMIN")
+  @UseGuards(JwtAuthGuard, RolesGuard)
   remove(@Param('id') id: number) {
     return this.service.delete(id);
   }
